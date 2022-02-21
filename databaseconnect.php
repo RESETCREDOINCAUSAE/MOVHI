156c95ac5ff2b4d4b10d59624f9df678c70529e9
RESETCREDOINCAUSAE <90111937+RESETCREDOINCAUSAE@users.noreply.github.com>
2022-02-21 16:05:22 -0500

    aaa

---
+++ databaseconnect.php
@@ -0,0 +1,15 @@
+<?php
+// Estos son los datos SQL en myPHPmyadmin
+$servername = "localhost";
+$database = "id17628488_movhi";
+$username = "id17628488_form";
+$password = "1233895437Babalon510.";
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $database);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
+mysqli_close($conn);
+?>
